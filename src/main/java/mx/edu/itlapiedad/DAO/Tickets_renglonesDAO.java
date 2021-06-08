d5c251d635d34ec01f0139050101a3ab82c185f3
Juan  Jose <juanjo23gallardo@gmail.com>
2021-06-08 01:14:36 -0500

    cambio de la clase Tickets_renglonesDAO

--- src/main/java/mx/edu/itlapiedad/DAO/Tickets_renglonesDAO.java
+++ src/main/java/mx/edu/itlapiedad/DAO/Tickets_renglonesDAO.java
@@ -1,17 +1,25 @@
 package mx.edu.itlapiedad.DAO;
 
+import java.sql.Timestamp;
 import java.util.List;
 
 import mx.edu.itlapiedad.models.Tickets_renglones;
+import mx.edu.itlapiedad.models.Ticket_renglones_importe;
 
 public interface Tickets_renglonesDAO {
-	List<Tickets_renglones> consultarTickets_renglones();
-	
-	Tickets_renglones buscar(int id);
-	
-	Tickets_renglones insertar(Tickets_renglones Tickets_renglones);
 
-	void actualizar(Tickets_renglones Tickets_renglones);
-	
+	Tickets_renglones buscar(int id);
+
+	Tickets_renglones insertar(Tickets_renglones tickets_renglones);
+
+	List<Tickets_renglones> consultarTickets_renglones();
+
+	void actualizar(Tickets_renglones tickets_renglones);
+
 	void eliminar(int id);
+
+	List<Ticket_renglones_importe> buscar_importe_cajero(int id);
+
+	List<Ticket_renglones_importe> buscar_importe_cajero_fecha(int id, Timestamp fecha_hora);
+
 }
