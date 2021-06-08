992b2dd2f1d0b4e76652432e9b62241df13a53f0
Juan  Jose <juanjo23gallardo@gmail.com>
2021-06-07 23:55:53 -0500

    creación en models de tickets importe

---
+++ src/main/java/mx/edu/itlapiedad/models/Ticket_renglones_importe.java
@@ -0,0 +1,14 @@
+package mx.edu.itlapiedad.models;
+
+public class Ticket_renglones_importe {
+
+	private float importe;
+
+	public float getImporte() {
+		return importe;
+	}
+
+	public void setImporte(float importe) {
+		this.importe = importe;
+	}
+}
